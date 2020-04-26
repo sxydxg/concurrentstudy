2be8d005f2dcd787acccf81c1508f027ee8df49a
rose <sxydxg@163.com>
2020-04-26 14:03:12 +0800

    测试线程池

--- src/main/java/dxg_threadPool/Test_ThreadPool3.java
+++ src/main/java/dxg_threadPool/Test_ThreadPool3.java
@@ -42,7 +42,8 @@ public class Test_ThreadPool3 {
 
         @Override
         public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
-            System.out.println("处理当一个人");
+            MyR task = (MyR)r ;
+            System.err.println(task.rName+" 被丢弃！");
         }
     }
 
@@ -50,9 +51,9 @@ public class Test_ThreadPool3 {
     public static void main(String[] args) {
 
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                5,
                 10,
-                20,
-                100,
+                1000,
                 TimeUnit.SECONDS,
                 new ArrayBlockingQueue<Runnable>(10),
                 new SelfThreadFactory(),
@@ -61,12 +62,35 @@ public class Test_ThreadPool3 {
         );
 
 
-        threadPoolExecutor.execute(new Runnable() {
+        //  10+10 = 20 ,当线程池扩充到最大线程，工作队列满了就会启用拒绝策略
+        //创建25个线程
+        //如果创建20个线程，则线程扩容到最大10，工作队列堆满10个任务，并不会触发拒绝策略
+        for (int i=1;i<=25;i++){
+            final int c = i ;
+            threadPoolExecutor.execute(new MyR("task"+c));
+        }
+
+    }
+
+
+    private static class MyR implements Runnable{
+
+        public MyR(String rName){
+            this.rName = rName ;
+        }
+        public String rName ;
+
+
             @Override
-            public void run() {
-                System.out.println("执行一个线程！");
+        public void run() {
+
+            System.out.println(Thread.currentThread().getName()+"  : "+rName+" "+" : 开始睡眠！");
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        });
+        }
 
     }
 }
